377adfa8976316bbb8960905e0e9036f7ebe0bc4
nicole trinity <nimble.embodiment.8@gmail.com>
2025-07-23 19:55:30 -0400

    fixed screenmessage

--- v7/screenmessage_manager.go
+++ v7/screenmessage_manager.go
@@ -1,10 +1,3 @@
-/**
-  *
-  *
-  *
-  *
-**/
-
 package sameriver
 
 type ScreenMessage interface {
@@ -16,13 +9,13 @@ type ScreenMessage interface {
 
 type FixedScreenMessage struct {
 	// the message to display
-	Msg string
+	Msg []string
 	// how many lines of text to show before needing the
 	// player to press space
 	Lines int
 	// age in milliseconds (used to scroll the text (if applicable)
 	// and to set inactive)
-	Age int
+	Age float64
 }
 
 type FloatingScreenMessage struct {
@@ -32,24 +25,46 @@ type FloatingScreenMessage struct {
 	// the bottom-left corner of the screen
 	Position [2]int
 	// how long the message should float for (in milliseconds)
-	Duration int
+	Duration float64
 	// used to time the disappearance of the message
-	Age int
+	Age float64
 }
 
 // responsible for spawning screen message entities
 // managing their lifecycles, and destroying their resources
 // when needed
 type ScreenMessageManager struct {
+	IDGen    IDGenerator
 	messages map[int]ScreenMessage
 }
 
-func (s *ScreenMessageManager) Init() {
+func NewScreenMessageManager() *ScreenMessageManager {
 	// arbitrary, can be tuned? Will grow
 	capacity := 4
-	s.messages = make(map[int]ScreenMessage, capacity)
+	return &ScreenMessageManager{
+		IDGen:    NewIDGenerator(),
+		messages: make(map[int]ScreenMessage, capacity),
+	}
 }
 
 func (s *ScreenMessageManager) Update(dt_ms int) {
-	// TODO: implement
+	for _, msg := range s.messages {
+		msg.Update(dt_ms)
+	}
+}
+
+func (s *ScreenMessageManager) Create(msg ScreenMessage) {
+	s.messages[s.IDGen.Next()] = msg
+}
+
+func (s *ScreenMessageManager) Destroy(id int) {
+	delete(s.messages, id)
+}
+
+func (s *ScreenMessageManager) Render() {
+	for id, msg := range s.messages {
+		if !msg.IsActive() {
+			delete(s.messages, id)
+		}
+	}
 }
